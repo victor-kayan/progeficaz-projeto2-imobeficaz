b673bc964a81bf880b916cf30728b56b5d6424bd
Victor Kayan <victor.vkal@gmail.com>
2026-09-16 20:55:58 -0300

    test: define listagem de imóveis
    Co-authored-by: Luigi Verdério <luigiverderio@hotmail.com>

---
+++ test_api.py
@@ -0,0 +1,56 @@
+import pytest
+from unittest.mock import MagicMock, patch
+
+from api import app
+
+
+@pytest.fixture
+def client():
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+@patch("api.connect_db")
+def test_lista_todos_os_imoveis(mock_connect_db, client):
+    # Given
+    mock_conn = MagicMock()
+    mock_cursor = MagicMock()
+    mock_conn.cursor.return_value = mock_cursor
+    mock_connect_db.return_value = mock_conn
+    mock_cursor.fetchall.return_value = [
+        (
+            1,
+            "Nicole Common",
+            "Travessa",
+            "Lake Danielle",
+            "Judymouth",
+            "85184",
+            "casa em condominio",
+            488423.52,
+            "2017-07-29",
+        )
+    ]
+
+    # When
+    response = client.get("/imoveis")
+
+    # Then
+    assert response.status_code == 200
+    assert response.get_json() == {
+        "imoveis": [
+            {
+                "id": 1,
+                "logradouro": "Nicole Common",
+                "tipo_logradouro": "Travessa",
+                "bairro": "Lake Danielle",
+                "cidade": "Judymouth",
+                "cep": "85184",
+                "tipo": "casa em condominio",
+                "valor": 488423.52,
+                "data_aquisicao": "2017-07-29",
+            }
+        ]
+    }
+    mock_cursor.execute.assert_called_once_with("SELECT * FROM imoveis")
